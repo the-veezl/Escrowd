ec4c4bfa302137c9b681abbf09ede84c90329cc4
xbuyan <kevobuyan@gmail.com>
2026-04-04 14:42:02 +0300

    Added package crypto

--- internal/crypto/hash.go
+++ internal/crypto/hash.go
@@ -1,6 +1,22 @@
 package crypto
 
 import (
-
-	
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 )
+
+func Hash(secret string) string {
+
+	bytes := sha256.Sum256([]byte(secret))
+
+	return hex.EncodeToString(bytes[:])
+}
+
+func GenerateSecret() string {
+
+	bytes := make([]byte, 16)
+
+	rand.Read(bytes)
+	return hex.EncodeToString(bytes)
+}
